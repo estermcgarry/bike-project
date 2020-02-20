397e5ee507da830cfce4daf2b4d72db58613053d
ester.mcgarry <ester.braganunesmcgarry@ucdconnect.ie>
2020-02-20 10:04:39 +0000

    testing API

--- hello.py
+++ hello.py
@@ -8,20 +8,20 @@ json_dataset = requests.get(url).text
 print(json_dataset)
 test = json.loads(json_dataset)
 
-# for i in test:
-#     #print(i)
-#     print('Number: ', i['number'])
-#     print('Name: ', i['name'])
-#     print('Address: ', i['address'])
-#     print('Position: ')
-#     print('Latitude: ', i['position']['lat'])
-#     print('Longitude: ', i['position']['lng'])
-#     print('Banking: ', i['banking'])
-#     print('Bonus: ', i['bonus'])
-#     print('Bike Stands: ', i['bike_stands'])
-#     print('Available bike stands: ', i['available_bike_stands'])
-#     print('Available bikes: ', i['available_bikes'])
-#     print()
+for i in test:
+    #print(i)
+    print('Number: ', i['number'])
+    print('Name: ', i['name'])
+    print('Address: ', i['address'])
+    print('Position: ')
+    print('Latitude: ', i['position']['lat'])
+    print('Longitude: ', i['position']['lng'])
+    print('Banking: ', i['banking'])
+    print('Bonus: ', i['bonus'])
+    print('Bike Stands: ', i['bike_stands'])
+    print('Available bike stands: ', i['available_bike_stands'])
+    print('Available bikes: ', i['available_bikes'])
+    print()
 
 
     #print(type(i))
@@ -34,3 +34,23 @@ json_dataset_weather = requests.get(url_weather).text
 
 with open("weather_api.json", "w") as outfile:
     outfile.write(json_dataset_weather)
+print(json_dataset_weather)
+
+weather_test = json.loads(json_dataset_weather)
+print(type(weather_test))
+for i in weather_test:
+    print(i)
+
+    # print(json_dataset_weather["list"])
+    # print('Number: ', i['number'])
+    # print('Name: ', i['name'])
+    # print('Address: ', i['address'])
+    # print('Position: ')
+    # print('Latitude: ', i['position']['lat'])
+    # print('Longitude: ', i['position']['lng'])
+    # print('Banking: ', i['banking'])
+    # print('Bonus: ', i['bonus'])
+    # print('Bike Stands: ', i['bike_stands'])
+    # print('Available bike stands: ', i['available_bike_stands'])
+    # print('Available bikes: ', i['available_bikes'])
+    # print()
