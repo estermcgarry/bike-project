4f5ea4a1e26a96d7850fc084a9cf682a9abb997e
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-03-05 09:33:55 +0000

    Basic Layout

--- test_sofia.py
+++ test_sofia.py
@@ -1,10 +1,46 @@
+from flask_googlemaps import GoogleMaps
+import mysql.connector
 from flask import Flask, render_template
+import json
 
-app = Flask(__name__)
+app = Flask(__name__,  template_folder="./templates")
+
+
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyBR7esRJtzTOqcv53Bk3t1xpiCF0YPO-3I"
+GoogleMaps(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def mapview():
+    mydb = mysql.connector.connect(
+        host="bailebikesdb.ck068lrxfgr6.us-east-1.rds.amazonaws.com",
+        user="admin",
+        passwd="picanha123",
+        database='BikeData'
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT StationName, Latitude, Longitude FROM StaticData;")
+
+    markers = []
+
+    for i in mycursor:
+        station = {"name" : i[0], "latitude" : i[1], "longitude" : i[2]}
+        markers.append(station)
+
+    mycursor.close()
+    mydb.close()
+
+    return render_template('index.html', markers=json.dumps(markers))
+
+
+@app.route('/stations')
+def stations():
+    return render_template('stations.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
