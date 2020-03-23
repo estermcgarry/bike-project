c9df73efc6a04a1abb256efe54d4b040aa78fa7a
ester.mcgarry <ester.braganunesmcgarry@ucdconnect.ie>
2020-03-23 22:11:06 +0000

    google maps

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ app.config['GOOGLEMAPS_KEY'] = "AIzaSyBR7esRJtzTOqcv53Bk3t1xpiCF0YPO-3I"
 # Initialize the extension
 GoogleMaps(app)
 
+
+
 @app.route('/')
 def mapview():
+
     mydb = mysql.connector.connect(
         host="bailebikesdb.ck068lrxfgr6.us-east-1.rds.amazonaws.com",
         user="admin",
@@ -19,21 +22,40 @@ def mapview():
         database='BikeData'
     )
 
-
     mycursor = mydb.cursor()
 
-    mycursor.execute("SELECT StationName, Latitude, Longitude FROM StaticData;")
+    mycursor.execute("SELECT StationNumber, StationName, Latitude, Longitude FROM StaticData;")
 
     markers = []
 
     for i in mycursor:
-        station = {"name" : i[0], "latitude" : i[1], "longitude" : i[2]}
+        station = {"number": i[0], "name" : i[1], "latitude" : i[2], "longitude" : i[3]}
         markers.append(station)
 
     mycursor.close()
     mydb.close()
 
     return render_template('index.html', markers=json.dumps(markers))
+
+@app.route('/station/<station>')
+def home(station):
+    mydb = mysql.connector.connect(
+        host="bailebikesdb.ck068lrxfgr6.us-east-1.rds.amazonaws.com",
+        user="admin",
+        passwd="picanha123",
+        database='BikeData'
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("select StationNumber, StationName, AvailableBikes, AvailableBikeStands from DynamicData where StationNumber = " + station + " order by time desc limit 1;")
+    message = "No information"
+    for i in mycursor:
+        message = "<b>" + i[1] + "</b> <br>Available bikes: " + str(i[2]) + " <br> Available bikestands: " + str(i[3])
+
+    mycursor.close()
+    mydb.close()
+    return message
+
 
 @app.route('/stations')
 def stations():
