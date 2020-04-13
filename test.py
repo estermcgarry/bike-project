1b113fe45b562e266ed84e681d5bfa29fe0c5e51
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-04-13 21:26:28 +0100

    Added test of how model would look

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from sklearn.externals import joblib
 # Load the model from the file 
 model_from_joblib = joblib.load('model.pkl')  
   
-X_test=[0,0,0,1,0,0,1,0]
+X_test=[[0,0,0,1,0,0,1,0]]
 
 # Use the loaded model to make predictions 
 model_from_joblib.predict(X_test) 
