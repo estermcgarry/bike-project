2c2cc7e3a6e96aa6cf915b50db615ca3add17896
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-04-15 15:11:08 +0100

    Putting port back to the local one

--- app.py
+++ app.py
@@ -109,8 +109,8 @@ if __name__ == '__main__':
     #app.run(host="0.0.0.0", port=80)
     
     #https
-    app.run(ssl_context='adhoc', port=443)
+    #app.run(ssl_context='adhoc', port=443)
 
     #locally
-    #app.run(debug=True, port=8000)
+    app.run(debug=True, port=8000)
 
