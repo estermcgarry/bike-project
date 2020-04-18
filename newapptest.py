a30107854d8b4967ca169524fc576a614cef8c91
Sofia Cruz <sofia.cruz@ucdconnect.ie>
2020-04-18 16:26:40 +0100

    Changed name dropdown

--- newapptest.py
+++
@@ -1,6 +0,0 @@
-from flask import request
-@app.route('/station/<station>/<weekday>', method=['POST'])
-def getvalue():
-    station = request.form["station"]
-    #weekday = request.form["station"]
-    return
